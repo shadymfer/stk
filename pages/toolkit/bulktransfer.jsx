4f82fca1e67e8a7fe7510e9bbea67eed32dcc1a8
gabster-code <88597385+gabster-code@users.noreply.github.com>
2022-09-29 06:00:07 +0800

    Update bulktransfer.jsx

--- pages/toolkit/bulktransfer.jsx
+++ pages/toolkit/bulktransfer.jsx
@@ -1,13 +1,13 @@
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import React from 'react'
-import BulkSidebar from '../../components/toolkit/ToolSideBar'
+
 
 const bulktransfer = () => {
   return (
     <div className='toolkit-global' >
       bulktransfer
       <WalletMultiButton className=''/>
-      <BulkSidebar />
+   
 
     </div>
   )
