ec412c8cbd4115657e6dc5a28d46307a05112251
gabster-code <88597385+gabster-code@users.noreply.github.com>
2022-09-29 03:56:23 +0800

    Update login.jsx

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -3,6 +3,7 @@ import {useSession, signIn, signOut} from 'next-auth/react'
 //import { providers, signIn, getSession, csrfToken } from "next-auth/client";
 
 import { FaDiscord } from 'react-icons/fa';
+import { TbArrowBackUp } from 'react-icons/tb';
 import logo from '../../public/assets/newth.ico'
 import Image from 'next/image';
 
@@ -17,11 +18,16 @@ const Login = ({ providers }) => {
           <div className='mt-[-200px] '>
           <Image src={logo} height={400} width={400}></Image>
             <h1>Welcome to the Shady Class Toolkit</h1>
-            <p className='pt-4'>Please sign in with your Discord</p>
+            <p className='pt-4 font-medium'>Please sign in with your Discord</p>
           </div>
           <div className='items-center p-5' >
               <button onClick={() => signIn('discord')}  className='text-white flex flex-row  justify-center items-center rounded-md uppercase bg-[#5865F2] h-[50px] w-64 text-center'> 
-                  <FaDiscord className='mr-2'> </FaDiscord> <p className=''>Login with Discord</p>
+                  <FaDiscord className='mr-2'> </FaDiscord> <p className='font-bold'>Login with Discord</p>
+              </button>
+          </div> 
+          <div className='items-center' >
+              <button className='text-white flex flex-row  justify-center items-center rounded-md uppercase bg-gradient-to-bl from-[#c31432] to-[#240b36] w-64 text-center h-12'> 
+                  <TbArrowBackUp className='mr-2'> </TbArrowBackUp> <p className=''>Go Back to Main Site</p>
               </button>
           </div> 
         </div>
